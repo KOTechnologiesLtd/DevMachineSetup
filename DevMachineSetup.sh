634f7580d81762425b5f478c4ca42c80a363378f
vijaykotechnologies <38214472+vijaykotechnologies@users.noreply.github.com>
2021-02-16 23:31:22 +0000

    As simple as ...
    As simple as ...

---
+++ DevMachineSetup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#python
+sudo apt-get install python3.6 unzip
+#pip
+sudo apt install python3-pip
+#git
+sudo apt-get install git
+#vscode
+sudo snap install --classic code
+#docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+#dockercompose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+#userdocker
+sudo usermod -aG docker $USER
+#awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+#PreCommit
+pip install pre-commit
+
